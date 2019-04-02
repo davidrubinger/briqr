a3142fbec6bae677995d1c35c8d8c723208b7d22
David Rubinger <david.rubinger@gmail.com>
2019-04-02 19:34:42 -0400

    Use mock API testing for users and groups

--- tests/testthat/test-bq_groups.R
+++ tests/testthat/test-bq_groups.R
@@ -4,3 +4,12 @@ test_that("Error when invalid inputs", {
     expect_error(bq_groups(organization = "", api_token = "my_token"))
     expect_error(bq_groups(organization = "my_org", api_token = ""))
 })
+
+with_mock_api({
+    test_that("Tibble is generated", {
+        groups <- bq_groups(
+            organization = "your-organization", api_token = "briq_api_token"
+        )
+        expect_is(groups, "tbl_df")
+    })
+})
--- tests/testthat/test-bq_users.R
+++ tests/testthat/test-bq_users.R
@@ -4,3 +4,12 @@ test_that("Error when invalid inputs", {
     expect_error(bq_users(organization = "", api_token = "my_token"))
     expect_error(bq_users(organization = "my_org", api_token = ""))
 })
+
+with_mock_api({
+    test_that("Tibble is generated", {
+        users <- bq_users(
+            organization = "your-organization", api_token = "briq_api_token"
+        )
+        expect_is(users, "tbl_df")
+    })
+})
